450f6647c0e84b8516eb970c1b7f9be1af1b3bd0
benjflowers <benjaminjflowers@gmail.com>
2025-04-11 14:45:29 -0500

    Drawing some shapes kinda

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use bevy::prelude::*;
-
+use bevy::{ prelude::*, input::mouse::MouseMotion, render::mesh::Mesh };
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, setup).chain())
         .add_systems(Update, mouse_button_input)
+        .add_systems(Update, mouse_motion)
         .run();
+}
+
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(50.0))),
+        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
+    ));
 }
 
 fn mouse_button_input(
@@ -16,4 +29,26 @@ fn mouse_button_input(
     if buttons.just_released(MouseButton::Left) {
         println!("Left mouse button released");
     }
+}
+
+fn mouse_motion(
+    mut event_reader: EventReader<MouseMotion>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut clear: ResMut<ClearColor>,
+) {
+    for event in event_reader.read() {
+        println!("Mouse moved: X: {} px, Y: {} px", event.delta.x, event.delta.y);
+        commands.spawn((
+            Mesh2d(meshes.add(Circle::new(50.0))),
+            MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
+            Transform::default().with_translation(Vec3::new(event.delta.x, event.delta.y, 0.0))
+        ));
+        clear.0 = Color::hsl(10.0, 10.0, 0.5);
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
