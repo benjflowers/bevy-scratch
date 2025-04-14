49d06e4e5e1860e9d1c2d218b8341d42abae7f2c
benjflowers <benjaminjflowers@gmail.com>
2025-04-14 18:18:57 -0500

    I'm an event dad now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,50 @@
-use bevy::{ prelude::*, input::mouse::MouseMotion, render::mesh::Mesh, window::PrimaryWindow };
+use bevy::{ prelude::*, input::mouse::MouseButton, render::mesh::Mesh, window::PrimaryWindow };
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_camera).chain())
-        .add_systems(Update, mouse_button_input)
+        .add_event::<SpawnCircleEvent>()
+        .add_systems(Startup, setup_camera)
+        .add_systems(Update, (mouse_input_system, spawn_circle_system))
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
+
+#[derive(Event)]
+struct SpawnCircleEvent {
+    position: Vec2,
+}
+
+fn mouse_input_system(
+    buttons: Res<ButtonInput<MouseButton>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    mut circle_events: EventWriter<SpawnCircleEvent>
+) {
+    if buttons.just_pressed(MouseButton::Left) {
+        let window = q_windows.single();
+        if let Some(position) = window.cursor_position() {
+            circle_events.send(SpawnCircleEvent {position});
+        } 
+    }
+}
+
+fn spawn_circle_system(
+    mut circle_events: EventReader<SpawnCircleEvent>
+) {
+    for event in circle_events.read() {
+        println!("Spawn circle at: {:?}", event.position);
+        // Here you would spawn the circle entity using the event.position
+        // For example:
+        // commands.spawn_bundle(SpriteBundle {
+        //     material: materials.add(ColorMaterial::from(Color::RED)),
+        //     transform: Transform::from_translation(Vec3::new(event.position.x, event.position.y, 0.0)),
+        //     ..Default::default()
+        // });
+    }
+}
+
 
 
 // fn setup(
@@ -23,26 +58,66 @@ fn setup_camera(mut commands: Commands) {
 //     ));
 // }
 
-fn mouse_button_input(
-    buttons: Res<ButtonInput<MouseButton>>,
-    q_windows: Query<&Window, With<PrimaryWindow>>
-) {
-    if buttons.just_pressed(MouseButton::Left) {
-        current_pos(q_windows);
-    }
-}
+// fn mouse_button_input(
+//     buttons: Res<ButtonInput<MouseButton>>,
+//     q_windows: Query<&Window, With<PrimaryWindow>>,
+//     commands: Commands,
+//     meshes: ResMut<Assets<Mesh>>,
+//     materials: ResMut<Assets<ColorMaterial>>,
+// ) {
+//     if buttons.just_pressed(MouseButton::Left) {
+//         let pos = current_pos(q_windows);
+//         let windowSize: Vec2 = get_resolution(Query<&Window, With<PrimaryWindow>>);
+//         let transform_calc = calc_pos_from_origin(pos, windowSize);
+//         spawn_circle(commands, meshes, materials,pos);
+//     }
+// }
 
-fn current_pos(
-    q_windows: Query<&Window, With<PrimaryWindow>>,
-) -> Vec2 {
-    if let Some(position) = q_windows.single().cursor_position() {
-        println!("Current Position {:?}", position);
-        return position;
-    } else {
-        println!("Click was outside of window");
-        return Vec2::ZERO;
-    }
-}
+// fn calc_pos_from_origin(pos: Vec2, windowSize: Vec2) {
+//     // window pos - top left is 0,0
+//     // origin pos - center is 0,0
+
+//     // if click pos is 50, 50
+//     // calculate window width and height
+//     // originPos is 1/2  of height and width always
+//     // clickPos - originPos = newPos
+//     println!("Window Size: {:?}", windowSize);
+//     println!("Click Position: {:?}", pos);
+// }
+
+// fn get_resolution(q_windows: Query<&Window, With<PrimaryWindow>>) -> Vec2 {
+//     let result;
+//     let window = q_windows.single();
+//     result = Vec2::new(window.width(), window.height());
+//     // println!("Window Size: {:?}", result);
+//     return result;
+// }
+
+// fn spawn_circle(
+//     mut commands: Commands,
+//     mut meshes: ResMut<Assets<Mesh>>,
+//     mut materials: ResMut<Assets<ColorMaterial>>,
+//     pos: Vec2,
+// ) {
+//     commands.spawn((
+//         Mesh2d(meshes.add(Circle::new(50.0))),
+//         MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
+//         // Transform::default().with_translation(Vec3::new(pos.x, pos.y, 0.0)),
+//     ));
+// }
+
+// fn current_pos(
+//     q_windows: Query<&Window, With<PrimaryWindow>>,
+// ) -> Vec2 {
+//     if let Some(position) = q_windows.single().cursor_position() {
+//         // println!("Current Position {:?
+//         // }", position);
+//         return position;
+//     } else {
+//         println!("Click was outside of window");
+//         return Vec2::ZERO;
+//     }
+// }
 
 // fn mouse_motion(
 //     mut event_reader: EventReader<MouseMotion>,
@@ -62,18 +137,18 @@ fn current_pos(
 //     }
 // }
 
-fn cursor_position(
-    q_windows: Query<&Window, With<PrimaryWindow>>,
-) -> (Vec2, ()) {
-    // Games typically only have one window (the primary window)
-    if let Some(position) = q_windows.single().cursor_position() {
-        println!("Cursor is inside the primary window, at {:?}", position);
-        return (position,());
-    } else {
-        println!("Cursor is not in the game window.");
-        return (Vec2::ZERO,());
-    }
-}
+// fn cursor_position(
+//     q_windows: Query<&Window, With<PrimaryWindow>>,
+// ) -> (Vec2, ()) {
+//     // Games typically only have one window (the primary window)
+//     if let Some(position) = q_windows.single().cursor_position() {
+//         println!("Cursor is inside the primary window, at {:?}", position);
+//         return (position,());
+//     } else {
+//         println!("Cursor is not in the game window.");
+//         return (Vec2::ZERO,());
+//     }
+// }
 
 // fn cursor_events(
 //     mut evr_cursor: EventReader<CursorMoved>,
