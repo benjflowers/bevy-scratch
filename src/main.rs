c2bca87c9604f70349f622ff2e0f862b4ad7eb3c
benjflowers <benjaminjflowers@gmail.com>
2025-04-17 21:01:29 -0500

    spawning shapes on mjouse as it moves

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{ prelude::*, input::mouse::MouseButton, render::mesh::Mesh, window::PrimaryWindow };
+use bevy::{ prelude::*, input::mouse::{ MouseButton, MouseMotion}, render::mesh::Mesh, window::PrimaryWindow };
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -22,22 +22,32 @@ struct SpawnCircleEvent {
 fn mouse_input_system(
     buttons: Res<ButtonInput<MouseButton>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
-    mut circle_events: EventWriter<SpawnCircleEvent>
+    mut circle_event_w: EventWriter<SpawnCircleEvent>,
+    mut cursor_event_r: EventReader<CursorMoved>,
 ) {
-    if buttons.just_pressed(MouseButton::Left) {
-        let window = q_windows.single();
-        if let Some(click_position) = window.cursor_position() {
-            // get window dimenstions
-            let window_size = Vec2::new(window.width(), window.height());
+    let window = q_windows.single();
+    // get window dimenstions
+    let window_size = Vec2::new(window.width(), window.height());
 
-            // calculated world position to pass to transform
-            let world_position = calculate_world_position(click_position, window_size);
-            circle_events.send(SpawnCircleEvent {
-                window_position: click_position,
+    if let Some(cursor_position) = window.cursor_position() {
+        let world_position = calculate_world_position(cursor_position, window_size);
+
+        if buttons.just_pressed(MouseButton::Left) {
+            circle_event_w.send(SpawnCircleEvent {
+                window_position: cursor_position,
                 world_position,
                 window_size,
             });
-        } 
+        }
+
+        for _event in cursor_event_r.read() {
+            // println!("Cursor moved to: {:?}", event.position);
+            circle_event_w.send(SpawnCircleEvent {
+                window_position: cursor_position,
+                world_position,
+                window_size,
+            });
+        }
     }
 }
 
@@ -50,7 +60,7 @@ fn spawn_circle_system(
     for event in circle_events.read() {
         println!("Spawn circle at: {:?}", event.world_position);
         commands.spawn((
-            Mesh2d(meshes.add(Circle::new(50.0))),
+            Mesh2d(meshes.add(Circle::new(10.0))),
             MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
             Transform::default().with_translation(Vec3::new(event.world_position.x, event.world_position.y, 0.0)),
         ));
@@ -63,106 +73,3 @@ fn calculate_world_position(click_position: Vec2, window_size: Vec2) -> Vec2 {
         window_size.y / 2.0 - click_position.y
     )
 }
-
-
-
-// fn setup(
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<ColorMaterial>>,
-// ) {
-//     commands.spawn((
-//         Mesh2d(meshes.add(Circle::new(50.0))),
-//         MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
-//     ));
-// }
-
-// fn mouse_button_input(
-//     buttons: Res<ButtonInput<MouseButton>>,
-//     q_windows: Query<&Window, With<PrimaryWindow>>,
-//     commands: Commands,
-//     meshes: ResMut<Assets<Mesh>>,
-//     materials: ResMut<Assets<ColorMaterial>>,
-// ) {
-//     if buttons.just_pressed(MouseButton::Left) {
-//         let pos = current_pos(q_windows);
-//         let windowSize: Vec2 = get_resolution(Query<&Window, With<PrimaryWindow>>);
-//         let transform_calc = calc_pos_from_origin(pos, windowSize);
-//         spawn_circle(commands, meshes, materials,pos);
-//     }
-// }
-
-// fn calc_pos_from_origin(pos: Vec2, windowSize: Vec2) {
-//     // window pos - top left is 0,0
-//     // origin pos - center is 0,0
-
-//     // if click pos is 50, 50
-//     // calculate window width and height
-//     // originPos is 1/2  of height and width always
-//     // clickPos - originPos = newPos
-//     println!("Window Size: {:?}", windowSize);
-//     println!("Click Position: {:?}", pos);
-// }
-
-// fn get_resolution(q_windows: Query<&Window, With<PrimaryWindow>>) -> Vec2 {
-//     let result;
-//     let window = q_windows.single();
-//     result = Vec2::new(window.width(), window.height());
-//     // println!("Window Size: {:?}", result);
-//     return result;
-// }
-
-// fn current_pos(
-//     q_windows: Query<&Window, With<PrimaryWindow>>,
-// ) -> Vec2 {
-//     if let Some(position) = q_windows.single().cursor_position() {
-//         // println!("Current Position {:?
-//         // }", position);
-//         return position;
-//     } else {
-//         println!("Click was outside of window");
-//         return Vec2::ZERO;
-//     }
-// }
-
-// fn mouse_motion(
-//     mut event_reader: EventReader<MouseMotion>,
-//     mut commands: Commands,
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut materials: ResMut<Assets<ColorMaterial>>,
-//     mut clear: ResMut<ClearColor>,
-// ) {
-//     for event in event_reader.read() {
-//         println!("Mouse moved: X: {} px, Y: {} px", event.delta.x, event.delta.y);
-//         commands.spawn((
-//             Mesh2d(meshes.add(Circle::new(50.0))),
-//             MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::hsl(40.0, 90.0, 0.5)))),
-//             Transform::default().with_translation(Vec3::new(event.delta.x, event.delta.y, 0.0))
-//         ));
-//         clear.0 = Color::hsl(10.0, 10.0, 0.5);
-//     }
-// }
-
-// fn cursor_position(
-//     q_windows: Query<&Window, With<PrimaryWindow>>,
-// ) -> (Vec2, ()) {
-//     // Games typically only have one window (the primary window)
-//     if let Some(position) = q_windows.single().cursor_position() {
-//         println!("Cursor is inside the primary window, at {:?}", position);
-//         return (position,());
-//     } else {
-//         println!("Cursor is not in the game window.");
-//         return (Vec2::ZERO,());
-//     }
-// }
-
-// fn cursor_events(
-//     mut evr_cursor: EventReader<CursorMoved>,
-// ) {
-//     for ev in evr_cursor.read() {
-//         println!(
-//             "New cursor position: X: {}, Y: {}, in Window ID: {:?}",
-//             ev.position.x, ev.position.y, ev.window
-//         );
-//     }
-// }
