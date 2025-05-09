6e0e6e2b8afdbc3f70cfea3b6d7d361cf6afff55
benjflowers <benjaminjflowers@gmail.com>
2025-05-08 23:21:01 -0400

    figuring out basics of observers and triggers

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,14 @@ struct Crawler{
     size: f32
 }
 
+#[derive(Event)]
+struct Crawl;
+
+#[derive(Event)]
+struct MoveCrawler{
+    pos: Vec2
+}
+
 impl Crawler {
     fn random(rand: &mut ChaCha8Rng) -> Self {
         Crawler {
@@ -37,4 +45,26 @@ fn setup(
 ) {
     // spawn camera
     commands.spawn(Camera2d);
+    let mut rng = ChaCha8Rng::seed_from_u64(19878367467713);
+    let mut observer = Observer::new(crawler_move);
+
+    for _ in 0..100 {
+        let entity = commands.spawn(Crawler::random(&mut rng)).id();
+        observer.watch_entity(entity);
+    }
+
+    commands.spawn(observer);
+}
+
+fn crawler_move(trigger: Trigger<Crawl>, query: Query<&Crawler>, mut commands: Commands) {
+    let id = trigger.entity();
+    let Some(mut entity) = commands.get_entity(id) else {
+        return;
+    };
+    let crawler = query.get(id).unwrap();
+
+    commands.trigger(MoveCrawler{
+        pos: crawler.pos
+    });
+    println!("{}", id);
 }
