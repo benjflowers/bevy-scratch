18e096679fdb72cacaf72b2b97993f0387208d14
benjflowers <benjaminjflowers@gmail.com>
2025-05-17 22:13:40 -0500

    adding button system and handling interactions

--- bevy_audio_analyser/src/ui.rs
+++ bevy_audio_analyser/src/ui.rs
@@ -1,4 +1,4 @@
-use bevy::prelude;
+use bevy::{prelude::*, color::palettes::basic::*};
 
 #[derive(Event)]
 pub struct AudioPlaybackEvent;
@@ -8,7 +8,50 @@ pub struct UiPlugin;
 impl Plugin for UiPlugin {
   fn build(&self, app: &mut App) {
     app.add_event::<AudioPlaybackEvent>()
-      .add_systems(Startup, spawn_ui);
+      .add_systems(Startup, spawn_ui)
+      .add_systems(Update, button_system);
+  }
+}
+
+fn spawn_ui(mut commands: Commands, assets: Res<AssetServer>) {
+  commands.spawn(button(&assets));
+}
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+fn button_system(
+  mut interaction_query: Query<
+      (
+          &Interaction,
+          &mut BackgroundColor,
+          &mut BorderColor,
+          &Children,
+      ),
+      (Changed<Interaction>, With<Button>),
+  >,
+  mut text_query: Query<&mut Text>,
+) {
+  for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+      let mut text = text_query.get_mut(children[0]).unwrap();
+      match *interaction {
+          Interaction::Pressed => {
+              **text = "PLAYING!".to_string();
+              *color = PRESSED_BUTTON.into();
+              border_color.0 = RED.into();
+          }
+          Interaction::Hovered => {
+              **text = "CAREFUL".to_string();
+              *color = HOVERED_BUTTON.into();
+              border_color.0 = Color::WHITE;
+          }
+          Interaction::None => {
+              **text = "PLAY".to_string();
+              *color = NORMAL_BUTTON.into();
+              border_color.0 = Color::BLACK;
+          }
+      }
   }
 }
 
