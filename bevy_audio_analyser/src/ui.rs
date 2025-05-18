1b198352c80573ac13de89d46ff57ab2bc17f4a1
benjflowers <benjaminjflowers@gmail.com>
2025-05-17 22:13:23 -0500

    defining button

--- bevy_audio_analyser/src/ui.rs
+++ bevy_audio_analyser/src/ui.rs
@@ -12,6 +12,40 @@ impl Plugin for UiPlugin {
   }
 }
 
-fn spawn_ui(mut commands: Commands) {
-  println!("UI STUFF");
+fn button(asset_server: &AssetServer) -> impl Bundle + use<> {
+  (
+      Node {
+          width: Val::Percent(100.0),
+          height: Val::Percent(100.0),
+          align_items: AlignItems::Center,
+          justify_content: JustifyContent::Center,
+          ..default()
+      },
+      children![(
+          Button,
+          Node {
+              width: Val::Px(150.0),
+              height: Val::Px(65.0),
+              border: UiRect::all(Val::Px(5.0)),
+              // horizontally center child text
+              justify_content: JustifyContent::Center,
+              // vertically center child text
+              align_items: AlignItems::Center,
+              ..default()
+          },
+          BorderColor(Color::BLACK),
+          BorderRadius::MAX,
+          BackgroundColor(NORMAL_BUTTON),
+          children![(
+              Text::new("PLAY"),
+              TextFont {
+                  font: asset_server.load("fonts/Unageo-Light.ttf"),
+                  font_size: 33.0,
+                  ..default()
+              },
+              TextColor(Color::srgb(0.9, 0.9, 0.9)),
+              TextShadow::default(),
+          )]
+      )],
+  )
 }
