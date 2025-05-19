21a4b038c87e6eb88f9c60478869170e93206509
benjflowers <benjaminjflowers@gmail.com>
2025-05-19 10:19:18 -0500

    adding load sound logic to setup function

--- bevy_audio_analyser/src/main.rs
+++ bevy_audio_analyser/src/main.rs
@@ -2,16 +2,24 @@ use bevy::prelude::*;
 
 mod ui;
 mod audio_player;
+mod audio_controller;
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
     .add_plugins(ui::UiPlugin)
     .add_plugins(audio_player::AudioPlayerPlugin)
+    .add_plugins(audio_controller::AudioControllerPlugin)
     .add_systems(Startup, setup)
     .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut audio_controller: ResMut<audio_controller::AudioController>,) {
   commands.spawn(Camera2d);
+
+  if let Err(e) = audio_controller.load_sound("assets/music/apr_13_agn.wav") {
+    error!("Failed to load audio: {}", e);
+  } else {
+    info!("Audio file loaded in setup");
+  }
 }
