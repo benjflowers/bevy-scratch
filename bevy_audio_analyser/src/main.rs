05dc093065b81aa10f59d92550b7a6641366190a
benjflowers <benjaminjflowers@gmail.com>
2025-05-18 21:16:29 -0500

    pulling plugin into main

--- bevy_audio_analyser/src/main.rs
+++ bevy_audio_analyser/src/main.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::*;
 
 mod ui;
+mod audio_player;
+
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
     .add_plugins(ui::UiPlugin)
+    .add_plugins(audio_player::AudioPlayerPlugin)
     .add_systems(Startup, setup)
     .run();
 }
