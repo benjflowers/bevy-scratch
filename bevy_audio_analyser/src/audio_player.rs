5f019ce4ed9ac6266993380adba894f06b68819b
benjflowers <benjaminjflowers@gmail.com>
2025-05-18 21:41:44 -0500

    playing audio when I click a button

--- bevy_audio_analyser/src/audio_player.rs
+++ bevy_audio_analyser/src/audio_player.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, audio::{AudioSource}};
+use bevy::{prelude::*, audio::{PlaybackSettings, AudioSource}};
 use crate::ui::AudioPlaybackEvent;
 
 pub struct AudioPlayerPlugin;
@@ -6,7 +6,8 @@ pub struct AudioPlayerPlugin;
 impl Plugin for AudioPlayerPlugin {
   fn build(&self, app: &mut App) {
     app.init_resource::<AudioAssets>()
-      .add_systems(Startup, load_audio);
+      .add_systems(Startup, load_audio)
+      .add_systems(Update, handle_play_button);
   }
 }
 
@@ -18,5 +19,21 @@ pub struct AudioAssets {
 fn load_audio(mut audio_assets: ResMut<AudioAssets>, asset_server: Res<AssetServer>) {
   audio_assets.music = Some(asset_server.load("music/apr_13_agn.wav"));
   println!("audio loaded!");
+}
+
+fn handle_play_button(
+  mut commands: Commands,
+  audio_assets: Res<AudioAssets>,
+  mut events: EventReader<AudioPlaybackEvent>,
+) {
+  for _ in events.read() {
+    if let Some(music) = &audio_assets.music {
+      commands.spawn(AudioPlayer::new(music.clone()));
+
+      info!("Playing audio!");
+    } else {
+      warn!("No audio loaded!");
+    }
+  }
 }
 
